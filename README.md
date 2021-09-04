3b9290ab4cd8db96344753f65321453c1021bdd9
John <John@DESKTOP-SRO1BKN>
2021-09-03 18:36:26 -0700

    Random ID assignment and user delete request added

--- README.md
+++ README.md
@@ -5,11 +5,14 @@ Practice building a REST API using NodeJS and Express
 Tech Used:
 NodeJS, Express, Postman
 
+Packages Used:
+uuid
+
 Acronym Meanings
 CRUD: Create Read Update Delete
 API: Application Programming Interface
 REST: Representational State Transfer
 
 Notes
-'get' and 'post' methods will have the parameters 'req' (request) and 'res' (response).
+'get' and 'post' requests will have the parameters 'req' (request) and 'res' (response).
 The response will be what is send to the web browser.
--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,38 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 // Initalize a router
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 25,
-    },
-    {
-        firstName: "Jane",
-        lastName: "Doe",
-        age: 24,
-    },
-];
+let users = [];
 
 // All routes here will start with /users as indicated in line 13 in index.js
 
-// Get method
 router.get("/", (req, res) => {
     res.send(users);
 });
 
-// Post method
 router.post("/", (req, res) => {
     // req.body is the contents of the post request
     const user = req.body;
+
+    // Creates new object with all the properties of users and an 'id' of 'uuidv4()'
     // This push method will add the user to the users array
-    users.push(user);
+    users.push({ ...user, id: uuidv4() });
+
     // Sends a message with confirmation that a user was added
     res.send(`User with the username ${user.firstName} added to the database`);
 });
 
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    users = users.filter((user) => user.id !== id);
+    res.send(`User with the id ${id} has been deleted.`);
+});
 export default router;
